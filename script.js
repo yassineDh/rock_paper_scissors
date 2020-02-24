d990b78c3c238392f149a30b09462fe2ca2016e0
YassineDH <yassine.dahane96@gmail.com>
2020-02-24 21:28:28 +0100

    complete the function playRound()

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ let moves = ["rock","paper","scissors"];
 
 function computerPlay(){
     let min = 0;
-    let max = moves.length();
+    let max = moves.length;
     let random =Math.floor(Math.random() * (max - min)) +min;
 
     return moves[random];
@@ -13,10 +13,32 @@ function playRound(playerSelection, computerSelection) {
     let playerSelectionUni = playerSelection.toLowerCase();
     let message;
 
-    if(playerSelection == computerSelection){
+    if(playerSelectionUni == computerSelection){
         message = "Its a tie";
     }else{
-        
+        switch(playerSelectionUni){
+            case "rock":
+                if(computerSelection == "paper"){
+                    message = "You Lose! Paper beats Rock";
+                }else{
+                    message = "You Win! Rock beats Scissors";
+                };
+                break;
+            case "paper":
+                if(computerSelection == "scissors"){
+                    message = "You Lose! Scissors beats Paper";
+                }else{
+                    message = "You Win! Paper beats Rock";
+                };
+                break;
+            case "scissors":
+                if(computerSelection == "rock"){
+                    message = "You Lose! Rock beats Scissors";
+                }else{
+                    message = "You Win! Scissors beats Paper";
+                };
+                break;
+        }
     }
-
-}
+    return message;
+};
