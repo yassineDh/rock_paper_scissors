30e114bcfb9d5c050021abad594395bad6e87836
YassineDH <yassine.dahane96@gmail.com>
2020-02-24 22:12:39 +0100

    complete the function game()

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
-let moves = ["rock","paper","scissors"];
+let moves = ["rock", "paper", "scissors"];
 
 
-function computerPlay(){
+function computerPlay() {
     let min = 0;
     let max = moves.length;
-    let random =Math.floor(Math.random() * (max - min)) +min;
+    let random = Math.floor(Math.random() * (max - min)) + min;
 
     return moves[random];
 };
@@ -12,33 +12,68 @@ function computerPlay(){
 function playRound(playerSelection, computerSelection) {
     let playerSelectionUni = playerSelection.toLowerCase();
     let message;
+    let win;
 
-    if(playerSelectionUni == computerSelection){
+    if (playerSelectionUni == computerSelection) {
         message = "Its a tie";
-    }else{
-        switch(playerSelectionUni){
+        win = 0;
+    } else {
+        switch (playerSelectionUni) {
             case "rock":
-                if(computerSelection == "paper"){
+                if (computerSelection == "paper") {
                     message = "You Lose! Paper beats Rock";
-                }else{
+                    win = -1;
+                } else {
                     message = "You Win! Rock beats Scissors";
+                    win = 1;
                 };
                 break;
             case "paper":
-                if(computerSelection == "scissors"){
+                if (computerSelection == "scissors") {
                     message = "You Lose! Scissors beats Paper";
-                }else{
+                    win = -1;
+                } else {
                     message = "You Win! Paper beats Rock";
+                    win = 1;
                 };
                 break;
             case "scissors":
-                if(computerSelection == "rock"){
+                if (computerSelection == "rock") {
                     message = "You Lose! Rock beats Scissors";
-                }else{
+                    win = -1;
+                } else {
                     message = "You Win! Scissors beats Paper";
+                    win = 1;
                 };
                 break;
         }
     }
-    return message;
+    return {
+            "message":message,
+            "win":win};
 };
+
+function game() {
+    let humans = 0;
+    let machine = 0;
+    let humanMove;
+    let machineMove;
+
+    for (let i = 0; i < 5; i++) {
+        machineMove = computerPlay();
+        humanMove = prompt("Choose one the following rock,paper or scissors");
+        let score = playRound(humanMove,machineMove)
+        switch(score.win){
+            case 1 :
+                humans++;
+                break;
+            case -1 :
+                machine++;
+                break;
+        };
+        console.log(score.message);
+        console.log("Humans : "+humans+" | Machines : "+machine);
+    };
+};
+
+game();
